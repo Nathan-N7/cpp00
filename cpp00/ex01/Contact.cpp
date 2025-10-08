b74e478c415707c2103b43c67690b0249dd85757
Nathan-N7 <nathan.rod1710@gmail.com>
2025-10-08 19:05:20 -0300

    espera argumento valido

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,17 +1,27 @@
-#include <iostream>
+#include "main.h"
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
 
 void	Contact::setContact()
 {
-	std::cout << "first name: ";
-	std::getline(std::cin, first_name);
-	std::cout << "last name: ";
-	std::getline(std::cin, last_name);
-	std::cout << "nickname: ";
-	std::getline(std::cin, nickname);
-	std::cout << "phone number: ";
-	std::getline (std::cin, phone_number);
-	std::cout << "darkset: ";
-	std::getline(std::cin, darkset);
+	while (first_name.empty()) {
+		std::cout << "first name: ";
+		std::getline(std::cin, first_name);
+	}
+	while (last_name.empty()) {
+		std::cout << "last name: ";
+		std::getline(std::cin, last_name);
+	}
+	while(nickname.empty()) {
+		std::cout << "nickname: ";
+		std::getline(std::cin, nickname);
+	}
+	while (phone_number.empty()) {
+		std::cout << "phone number: ";
+		std::getline (std::cin, phone_number);
+	}
+	while (darkset.empty()) {
+		std::cout << "darkset: ";
+		std::getline(std::cin, darkset);
+	}
 }
---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,3 @@
+#include "main.h"
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
