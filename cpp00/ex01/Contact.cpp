65bde3905700e951f68978c5a6aab71b82f98d20
Nathan-N7 <nathan.rod1710@gmail.com>
2025-10-08 19:18:33 -0300

    verif espaço ou tab no começo

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -4,23 +4,23 @@
 
 void	Contact::setContact()
 {
-	while (first_name.empty()) {
+	while (first_name.empty() || first_name.find_first_not_of(" \t")) {
 		std::cout << "first name: ";
 		std::getline(std::cin, first_name);
 	}
-	while (last_name.empty()) {
+	while (last_name.empty() || last_name.find_first_not_of(" \t")) {
 		std::cout << "last name: ";
 		std::getline(std::cin, last_name);
 	}
-	while(nickname.empty()) {
+	while(nickname.empty() || nickname.find_first_not_of(" \t")) {
 		std::cout << "nickname: ";
 		std::getline(std::cin, nickname);
 	}
-	while (phone_number.empty()) {
+	while (phone_number.empty() || phone_number.find_first_not_of(" \t")) {
 		std::cout << "phone number: ";
 		std::getline (std::cin, phone_number);
 	}
-	while (darkset.empty()) {
+	while (darkset.empty() || darkset.find_first_not_of(" \t")) {
 		std::cout << "darkset: ";
 		std::getline(std::cin, darkset);
 	}
