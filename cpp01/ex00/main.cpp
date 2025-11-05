1071909d92e444759101fa30d487ff894922d3e3
Nathan-N7 <nathan.rod1710@gmail.com>
2025-11-05 19:57:01 -0300

    ex00 finalizado

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+int main(void) {
+	Zombie *heapZombie = newZombie("HeapZombie");
+	heapZombie->announce();
+	delete (heapZombie);
+	randomChump("StackZombie");
+	return (0);
+}
